1f3a6bfd286a834d997251af7909830b1a997b3f
Kristiyan Manov <93081298+KristiyanManow@users.noreply.github.com>
2022-01-11 13:06:12 +0200

    Using-the-native-event-object

---
+++ Using-the-native-event-object/app.js
@@ -0,0 +1,43 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      lastName: "",
+      //fullname: "",
+    };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    },
+  },
+  computed: {
+    fullname() {
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
+  },
+  methods: {
+    resetValue() {
+      this.name = "";
+      this.lastName = "";
+    },
+    setName(event, lastName) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+  },
+});
+
+app.mount("#events");
